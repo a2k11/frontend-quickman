9a2601f164f897e07303e8fa8362fbb0e75cef1a
Adam Kaewell <adam.kaewell@gmail.com>
2020-12-30 00:34:18 -0500

    added link, styled components Logo & StyledHeader to Header component, fixed theme spelling

--- components/Page.js
+++ components/Page.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 const theme = {
-  red: '#FF0000',
+  pink: '#ff91a4',
   black: '#393939',
   grey: '#3A3A3A',
-  lightgrey: '#E1E1E1',
+  lightGrey: '#E1E1E1',
   offWhite: '#EDEDED',
   maxWidth: '1000px',
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
@@ -15,11 +15,11 @@ const theme = {
 
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
 `;
@@ -28,11 +28,13 @@ const Inner = styled.div`
 class Page extends Component {
   render() {
     return (
-      <StyledPage>
-        <Meta />
-        <Header />
-        <Inner>{this.props.children}</Inner>
-      </StyledPage>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
