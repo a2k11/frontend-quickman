2f875c8192eb29da49eb14b24cddee4c2546ad10
Adam Kaewell <adam.kaewell@gmail.com>
2020-12-30 23:59:13 -0500

    adding font, injectGlobal into Page

--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 const theme = {
   pink: '#ff91a4',
@@ -22,6 +22,30 @@ const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'Roboto', sans-serif;
+    src: url()('/static/Roboto-Medium.ttf')
+    format('ttf');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'Roboto', sans-serif;
+  }
 `;
 
 
