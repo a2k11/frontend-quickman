a02036fc10db1dbd1b5236a99311f5c89b434ff0
a2k11 <akaewell@xometry.com>
2021-01-15 22:37:48 -0500

    setting up mutation, exporting appropriate components & query & mutation from CreateItem / Items

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -4,13 +4,33 @@ import gql from 'graphql-tag';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 
+const createItemMutation = gql`
+  mutation createItemMutation(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
+
 class CreateItem extends Component {
   state = {
-    title: '',
-    description: '',
-    image: '',
-    largeImage: '',
-    price: 0,
+    title: 'shoes',
+    description: 'Fresh new shoes',
+    image: 'shoe.jpg',
+    largeImage: 'largeShoe.jpg',
+    price: 1110,
   };
 
   handleChange = e => {
@@ -21,49 +41,56 @@ class CreateItem extends Component {
 
   render() {
     return (
-      <Form>
-        <fieldset>
-          <label htmlFor="title">
-            Title
-            <input
-              type="text"
-              id="title"
-              name="title"
-              placeholder="Title"
-              value={this.state.title}
-              required
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="price">
-            Price
-            <input
-              type="number"
-              id="price"
-              name="price"
-              placeholder="Price"
-              value={this.state.price}
-              required
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="price">
-            Description
-            <textarea
-              id="description"
-              name="description"
-              placeholder="Enter a Description"
-              value={this.state.description}
-              required
-              onChange={this.handleChange}
-            />
-          </label>
-
-
-        </fieldset>
-      </Form>
+      <Mutation mutation={createItemMutation} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form onSubmit={(e) => {
+            e.preventDefault();
+            console.log(this.state);
+          }}>
+            <fieldset>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  value={this.state.title}
+                  required
+                  onChange={this.handleChange}
+                  />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  value={this.state.price}
+                  required
+                  onChange={this.handleChange}
+                  />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter a Description"
+                  value={this.state.description}
+                  required
+                  onChange={this.handleChange}
+                  />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
     )
   }
 }
 
 export default CreateItem;
+export { createItemMutation };
