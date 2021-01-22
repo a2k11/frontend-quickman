8e9d8becd21f88afd6bd4b997bb51a3e15ea92a6
a2k11 <akaewell@xometry.com>
2021-01-21 23:51:40 -0500

    moving import

--- components/Page.js
+++ components/Page.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
+import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
 
